54810141b599ee021f908708e29d36750be4e60d
Beyza Nur <beyzanurelitok21@gmail.com>
2025-01-06 15:21:37 +0300

    tried to fixing the problem

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,38 +3,38 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/beyzanurelitok/profiles-rest-api1.git'
+PROJECT_GIT_URL='https://github.com/SeherKumsar/profiles-rest-api.git'
 
-PROJECT_BASE_PATH='C:\Users\Beyza Nur\Course1\profiles-rest-api1'
+PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
-# Set Ubuntu Language
-locale-gen en_GB.UTF-8
-
-# Install Python, SQLite and pip
 echo "Installing dependencies..."
 apt-get update
-apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git
+apt-get install -y python3-dev python3-venv sqlite python3-pip supervisor nginx git
 
-
-#Create virtual environment
+# Create project directory
 mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
+# Create virtual environment
+mkdir -p $PROJECT_BASE_PATH/env
 python3 -m venv $PROJECT_BASE_PATH/env
 
-#Install python packages
-$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.21
+# Install python packages
+$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
+$PROJECT_BASE_PATH/env/bin/pip install uwsgi==2.0.18
 
-# Run migrations
-$PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
-#$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
-# Setup Supervisor to run our uwsgi process.
+# Run migrations and collectstatic
+cd $PROJECT_BASE_PATH
+$PROJECT_BASE_PATH/env/bin/python manage.py migrate
+$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
+
+# Configure supervisor
 cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
 supervisorctl reread
 supervisorctl update
 supervisorctl restart profiles_api
 
-# Setup nginx to make our application accessible.
+# Configure nginx
 cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
